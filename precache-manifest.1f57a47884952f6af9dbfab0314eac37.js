025cd827a7c381f858ba93af0131928d54759fdf
Kevin Rodrigo Guerrero Posso <63936270+RenSunk@users.noreply.github.com>
2022-08-22 12:43:11 -0500

    Updates

---
+++ precache-manifest.1f57a47884952f6af9dbfab0314eac37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02327060dd4a4ba73e38",
+    "url": "/PokeDex/static/css/main.6b30a99c.chunk.css"
+  },
+  {
+    "revision": "02327060dd4a4ba73e38",
+    "url": "/PokeDex/static/js/main.02327060.chunk.js"
+  },
+  {
+    "revision": "15811bbf0fd42262f03b",
+    "url": "/PokeDex/static/js/1.15811bbf.chunk.js"
+  },
+  {
+    "revision": "a47a4e12f151ab30b598",
+    "url": "/PokeDex/static/js/runtime~main.a47a4e12.js"
+  },
+  {
+    "revision": "b9a266f2a3650e07a737ab4ad272b85c",
+    "url": "/PokeDex/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/PokeDex/precache-manifest.1f57a47884952f6af9dbfab0314eac37.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/PokeDex/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
